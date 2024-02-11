b4bbc63467e381d0897c6fbc2ad6b19bef9185bb
Daniil Itsyxon <daniilitsyxon@gmail.com>
2024-02-11 08:32:23 +0300

    fixes

--- js/main.js
+++ js/main.js
@@ -5,30 +5,6 @@ menuBtn.addEventListener('click', () => {
   menu.classList.toggle('menu-active')
 })
 
-const anchors = [].slice.call(document.querySelectorAll('a[href*="#"]')),
-  animationTime = 300,
-  framesCount = 20
-anchors.forEach(function (item) {
-  item.addEventListener('click', function (e) {
-    e.preventDefault()
-    let coordY =
-      document.querySelector(item.getAttribute('href')).getBoundingClientRect()
-        .top + window.pageYOffset
-    let scroller = setInterval(function () {
-      let scrollBy = coordY / framesCount
-      if (
-        scrollBy > window.pageYOffset - coordY &&
-        window.innerHeight + window.pageYOffset < document.body.offsetHeight
-      ) {
-        window.scrollBy(0, scrollBy)
-      } else {
-        window.scrollTo(0, coordY)
-        clearInterval(scroller)
-      }
-    }, animationTime / framesCount)
-  })
-})
-
 const modalWindow = document.querySelector('.modal')
 
 const openModal = document.querySelectorAll('.single__button')
@@ -46,7 +22,7 @@ contactButton.forEach((contact) => {
 })
 
 const modalButton = document.querySelector('.modal__button')
-modalButton.addEventListener('click', (e) => {
+modalButton.addEventListener('click', () => {
   setTimeout(() => {
     modalWindow.classList.remove('open')
   }, 100)
@@ -76,4 +52,28 @@ document
 document.querySelector('.modal').addEventListener('click', (e) => {
   if (e._isClickWithInModal) return
   e.currentTarget.classList.remove('open')
+})
+
+const anchors = [].slice.call(document.querySelectorAll('a[href*="#"]')),
+  animationTime = 300,
+  framesCount = 20
+anchors.forEach(function (item) {
+  item.addEventListener('click', function (e) {
+    e.preventDefault()
+    let coordY =
+      document.querySelector(item.getAttribute('href')).getBoundingClientRect()
+        .top + window.pageYOffset
+    let scroller = setInterval(function () {
+      let scrollBy = coordY / framesCount
+      if (
+        scrollBy > window.pageYOffset - coordY &&
+        window.innerHeight + window.pageYOffset < document.body.offsetHeight
+      ) {
+        window.scrollBy(0, scrollBy)
+      } else {
+        window.scrollTo(0, coordY)
+        clearInterval(scroller)
+      }
+    }, animationTime / framesCount)
+  })
 })

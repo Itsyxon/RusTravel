2a5cf8583d1464725da52d89e3f1d305cbdc55b5
Daniil Itsyxon <daniilitsyxon@gmail.com>
2024-02-11 08:25:10 +0300

    Добавлены модальные окна + несколько городов

--- js/main.js
+++ js/main.js
@@ -28,3 +28,52 @@ anchors.forEach(function (item) {
     }, animationTime / framesCount)
   })
 })
+
+const modalWindow = document.querySelector('.modal')
+
+const openModal = document.querySelectorAll('.single__button')
+openModal.forEach((button) => {
+  button.addEventListener('click', () => {
+    modalWindow.classList.add('open')
+  })
+})
+
+const contactButton = document.querySelectorAll('.contact__button')
+contactButton.forEach((contact) => {
+  contact.addEventListener('click', () => {
+    modalWindow.classList.add('open')
+  })
+})
+
+const modalButton = document.querySelector('.modal__button')
+modalButton.addEventListener('click', (e) => {
+  setTimeout(() => {
+    modalWindow.classList.remove('open')
+  }, 100)
+})
+
+const modalClose = document.querySelector('.modal__close')
+modalClose.addEventListener('click', () => {
+  setTimeout(() => {
+    modalWindow.classList.remove('open')
+  }, 100)
+})
+
+window.addEventListener('keydown', (e) => {
+  if (e.key === 'Escape') {
+    setTimeout(() => {
+      modalWindow.classList.remove('open')
+    }, 100)
+  }
+})
+
+document
+  .querySelector('.modal .modal__window')
+  .addEventListener('click', (e) => {
+    e._isClickWithInModal = true
+  })
+
+document.querySelector('.modal').addEventListener('click', (e) => {
+  if (e._isClickWithInModal) return
+  e.currentTarget.classList.remove('open')
+})
